38c119b1f783cad5ca913cbed2b7b3a5f337674c
Charley <charley1135@gmail.com>
2023-02-15 20:21:33 -0800

    feat: Added Array functions + structs.

--- Contracts/SimpleStorage.sol
+++ Contracts/SimpleStorage.sol
@@ -3,11 +3,45 @@ pragma solidity 0.8.7; // stable version
 
 contract SimpleStorage {
     // this gets initialized to zero!
-    uint256 public favoriteNumber;
+    uint256 favoriteNumber;
+    // Ex using variable creation: People public person = People({favoriteNumber: 2, name: "Charley"});
+    
+    // Creating a struct of people. People object.
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    //uint256[] public favoriteNumbersList;
+    People[] public people;
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        // 1st way
+        // people.push(People(_favoriteNumber, _name));  
+        //2nd way
+        // People memory newPerson = People({favoriteNumber: _favoriteNumber, name: _name});
+        // people.push(newPerson);
+        //3rd way
+        People memory newPerson = People(_favoriteNumber, _name);
+        people.push(newPerson);
+    }
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
+
     }
+
+    // view & pure functions don't cost gas
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+    // pure function
+    function add() public pure returns (uint256) {
+        return (1+1);
+    }
+
+
+
 
 }
 
